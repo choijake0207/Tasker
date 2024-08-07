b67bdc28df13cdfbcf0a033ea32cd34102e84940
choijake0207 <jake.ch0712@gmail.com>
2024-08-07 18:13:33 -0400

    create routes and render new routes based off projects array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import RootLayout from './Layouts/RootLayout'
+import { ProjectContext } from './Context/ProjectContext'
 
 export default function App() {
+  const {projects} = useContext(ProjectContext)
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout/>}>
+        {
+          projects.map(project => {
+            return (
+              <Route
+                key={project.id}
+                path={`/projects/${project.name}`}
+              />
+            )
+          })
+        }
+      </Route>
+    )
+  )
+
+
+
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
