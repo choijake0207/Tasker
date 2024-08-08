72bc8b07b991443e6e22c6da5c809fb6a552cb63
choijake0207 <jake.ch0712@gmail.com>
2024-08-08 11:42:52 -0400

    initialize global state from either saved local storage data or template
    add useEffect to save state changes to storage

--- src/Context/ProjectContext.jsx
+++ src/Context/ProjectContext.jsx
@@ -1,4 +1,4 @@
-import React, {createContext, useState} from 'react'
+import React, {createContext, useEffect, useState} from 'react'
 import { initialProjects } from '../Data/InitialData'
 
 export const ProjectContext = createContext()
@@ -7,7 +7,21 @@ export const ProjectContext = createContext()
 
 export function ContextProvider({children}) {
 
-    const [projects, setProjects] = useState(initialProjects)
+  // initialize state from either saved data or blank template
+    const [projects, setProjects] = useState(() => {
+      const savedProjects = localStorage.getItem("projects")
+      if (savedProjects) {
+        return JSON.parse(savedProjects)
+      } else {
+        localStorage.setItem("projects", JSON.stringify(initialProjects))
+        return initialProjects
+      }
+    })
+
+    // save data change to local storage
+    useEffect(() => {
+      localStorage.setItem("projects", JSON.stringify(projects))
+    }, [projects])
 
 
 
