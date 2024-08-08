55467e96989391693a735758b7426107cdeafcc9
choijake0207 <jake.ch0712@gmail.com>
2024-08-08 13:01:47 -0400

    add task header with priortiy/category tags

--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./Task.css"
 
-export default function Task({task}) {
+export default function Task({task, color}) {
   return (
-    <div className="task">
-        {task.info}
+    <div className="task" style={{borderLeft: `3px solid ${color}`}}>
+      <header className="task-header">
+        <p className={`task-priority ${task.priority}`}>{task.priority}</p>
+        {task.category && <p className="task-category">{task.category}</p> }
+      </header>
+        <p>{task.info}</p>
     </div>
   )
 }
