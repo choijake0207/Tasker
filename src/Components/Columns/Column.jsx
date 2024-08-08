24b5c36cfaee21c230f5555b5a07ba9b710153ed
choijake0207 <jake.ch0712@gmail.com>
2024-08-07 20:01:27 -0400

    receive tasks prop, use it to initialize local tasks state, render w map

--- src/Components/Columns/Column.jsx
+++ src/Components/Columns/Column.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "./Column.css"
 
-export default function Column({column}) {
+export default function Column({column, tasks}) {
+
+  
+  const [columnTasks, setColumnTasks] = useState(tasks)
+
   return (
     <div className="column">
         <header>
             <h3>{column.name}</h3>
         </header>
+        <main>
+          {columnTasks.map(task => {
+            return (
+              <p>{task.info}</p>
+            )
+          })}
+         
+        </main>
     </div>
   )
 }
