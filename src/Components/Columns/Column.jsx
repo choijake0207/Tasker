c3593c397e7a3ee6ab9c0aaba2c1726b58595fcc
choijake0207 <jake.ch0712@gmail.com>
2024-08-08 12:22:33 -0400

    add display of task count in header

--- src/Components/Columns/Column.jsx
+++ src/Components/Columns/Column.jsx
@@ -1,15 +1,23 @@
 import React, {useState} from 'react'
 import Task from '../Task/Task'
 import "./Column.css"
+import { Circle } from 'phosphor-react'
+
 
 export default function Column({column, tasks}) {
 
   
-
+ 
   return (
     <div className="column">
-        <header>
-            <h3>{column.name}</h3>
+        <header className="column-header">
+            <div className="column-header-name">
+              <Circle size={".75em"} weight="fill"/>
+              <p>{column.name}</p>
+              <p>( {tasks.length} )</p>
+            </div>
+            
+            <button> + Add Task</button>
         </header>
         <main>
           {tasks.map(task => {
