f39779e749f1292d9fae7a287444fbb617a613a0
choijake0207 <jake.ch0712@gmail.com>
2024-08-11 13:30:23 -0400

    add navlink for projects page

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react'
 import {Outlet, NavLink} from "react-router-dom"
 import { ProjectContext } from '../Context/ProjectContext'
-import { Note, FolderSimple } from 'phosphor-react'
+import { Note, FolderSimple, House } from 'phosphor-react'
 
 
 
@@ -11,18 +11,19 @@ export default function RootLayout() {
     <div className="root-layout">
         <nav className="side-nav-bar">
             <h1><Note/>tasker</h1>
-            <NavLink to="/">Home</NavLink>
-            <div className="project-container">
-                <p><FolderSimple/>PROJECTS</p>
-                {projects.map(project => {
-                    return (
-                        <NavLink to={`/projects/${project.name}`} key={project.id}>
-                            - {project.name}
-                        </NavLink>
-                    )
-                })}
+            <div className="links-container">
+                <NavLink to="/">Home</NavLink>
+                <div className="project-container">
+                    <NavLink to="/projects">Projects</NavLink>
+                    {projects.map(project => {
+                        return (
+                            <NavLink to={`/projects/${project.name}`} key={project.id}>
+                                {project.name}
+                            </NavLink>
+                        )
+                    })}
+                </div>
             </div>
-         
         </nav>
         <main className="main-screen">
             <Outlet/>
