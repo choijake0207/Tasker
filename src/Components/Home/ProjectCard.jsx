d965833ac42dafa9b802a0c1151848b964514c44
choijake0207 <jake.ch0712@gmail.com>
2024-08-11 17:26:10 -0400

    change clickable zone to exclude button

--- src/Components/Home/ProjectCard.jsx
+++ src/Components/Home/ProjectCard.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
+import { DotsThreeCircle } from "phosphor-react"
 
 export default function ProjectCard ({project}) {
     const navigate = useNavigate()
     return (
-        <div className="project-card" onClick={() => navigate(`/projects/${project.name}`)}>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
+        <div className="project-card">
+            <button><DotsThreeCircle size={"2em"}/></button>
+            <div onClick={() => navigate(`/projects/${project.name}`) }>
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+            </div>
         </div>
     )
 }
