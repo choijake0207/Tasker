ef558bf0479c24e2497e7605a0e841c2d66475e0
choijake0207 <jake.ch0712@gmail.com>
2024-08-11 16:34:49 -0400

    add change handler and call addProject function from context

--- src/Components/Forms/ProjectForm.jsx
+++ src/Components/Forms/ProjectForm.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
-
+import React, {useState, useContext} from 'react'
+import { projectTemplate } from '../../Data/InitialData'
+import { ProjectContext } from '../../Context/ProjectContext'
 
 export default function ProjectForm({onClose}) {
+    const [newProject, setNewProject] = useState(projectTemplate)
+    const {addProject} = useContext(ProjectContext)
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setNewProject(prev => ({...prev, [name]: value}))
+    }
+    const createProject = (e) => {
+        e.preventDefault()
+        const project = {...newProject, id: crypto.randomUUID()}
+        addProject(project)
+        setNewProject(projectTemplate)
+        onClose()
+    }
   return (
     <div className="form-overlay">
         <div className="form-container">
             <h1>Create A New Project</h1>
-            <form className="form">
+            <form className="form" onSubmit={createProject}>
                 <label>Project Name</label>
-                <input/>
+                <input
+                    type="text"
+                    value={newProject.name}
+                    name="name"
+                    onChange={handleChange}
+                    required
+                />
                 <label>Project Description (optional)</label>
-                <textarea/>
+                <textarea
+                    value={newProject.description}
+                    name="description"
+                    onChange={handleChange}
+                />
                 <div className="form-btn-container">
                     <button className="close-btn"type="button" onClick={onClose}>Close</button>
                     <button className="submit-btn"type="submit">Create</button>
