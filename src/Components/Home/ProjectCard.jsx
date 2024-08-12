2d06bfd5709fa1b445042f1128a7359db7b1437d
choijake0207 <jake.ch0712@gmail.com>
2024-08-12 15:52:21 -0400

    change button icon and resize

--- src/Components/Home/ProjectCard.jsx
+++ src/Components/Home/ProjectCard.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
-import { DotsThreeCircle } from "phosphor-react"
+import { DotsThree } from "phosphor-react"
 
 export default function ProjectCard ({project}) {
     const navigate = useNavigate()
     return (
         <div className="project-card">
-            <button><DotsThreeCircle size={"2em"}/></button>
+            <button><DotsThree size={"2em"}/></button>
             <div onClick={() => navigate(`/projects/${project.name}`) }>
                 <h3>{project.name}</h3>
                 <p>{project.description}</p>
