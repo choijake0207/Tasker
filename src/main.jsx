d49208c6f8aed73cb398e1384f8d337421b7ebed
choijake0207 <jake.ch0712@gmail.com>
2024-08-14 15:30:37 -0700

    wrap app in dnd context

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ContextProvider } from './Context/ProjectContext.jsx'
+import { DndContext } from '@dnd-kit/core'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <DndContext>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </DndContext>
   </StrictMode>,
 )
