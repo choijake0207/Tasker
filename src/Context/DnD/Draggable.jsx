9636b9d2f8305a5a3b1ebfc720c2081734ac7f8b
choijake0207 <jake.ch0712@gmail.com>
2024-08-14 16:47:15 -0700

    create draggable/droppable wrappers for task and column components

---
+++ src/Context/DnD/Draggable.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useDraggable } from '@dnd-kit/core'
+
+export default function Draggable({children, taskId}) {
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: taskId
+      })
+    const style = transform ? {transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`} : undefined
+
+  return (
+    <div style={style} ref={setNodeRef} {...listeners} {...attributes}>{children}</div>
+  )
+}
---
+++ src/Context/DnD/Droppable.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useDroppable } from '@dnd-kit/core'
+
+export default function Droppable({children, columnId}) {
+    const {isOver, setNodeRef} = useDroppable({
+        id: columnId
+    })
+    const style = {background: isOver ? "rgb(56,59,68)" : null}
+    
+  return (
+    <div ref={setNodeRef} style={style}>{children}</div>
+  )
+}
