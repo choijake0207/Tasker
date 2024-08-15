9951aeb44fc4d2312c7d9c65896249b548402f6f
choijake0207 <jake.ch0712@gmail.com>
2024-08-15 15:06:29 -0700

    add conditional class if component is being dragged

--- src/Context/DnD/Draggable.jsx
+++ src/Context/DnD/Draggable.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { useDraggable } from '@dnd-kit/core'
 
 export default function Draggable({children, taskId}) {
-    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    const {attributes, listeners, setNodeRef, transform, isDragging} = useDraggable({
         id: `task/${taskId}`
       })
     const style = transform ? {transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`} : undefined
 
   return (
-    <div style={style} ref={setNodeRef} {...listeners} {...attributes}>{children}</div>
+    <div style={style} ref={setNodeRef} {...listeners} {...attributes} className={isDragging && "draggable"}>{children}</div>
   )
 }
