f0e2e0b3637893b4d024a60a9bc96daad6d20749
choijake0207 <jake.ch0712@gmail.com>
2024-08-15 14:51:55 -0700

    remove isOver styles and add conditional class to wrapper

--- src/Context/DnD/Droppable.jsx
+++ src/Context/DnD/Droppable.jsx
@@ -5,9 +5,9 @@ export default function Droppable({children, columnId}) {
     const {isOver, setNodeRef} = useDroppable({
         id: `column/${columnId}`
     })
-    const style = {background: isOver ? "rgb(56,59,68)" : null}
+
     
   return (
-    <div ref={setNodeRef} style={style}>{children}</div>
+    <div ref={setNodeRef} className={isOver && "droppable"}>{children}</div>
   )
 }
