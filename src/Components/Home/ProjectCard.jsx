855c915f542608b4538e83924bcb7386fba652ae
choijake0207 <jake.ch0712@gmail.com>
2024-08-19 14:07:22 -0700

    import menu and add toggle logic

--- src/Components/Home/ProjectCard.jsx
+++ src/Components/Home/ProjectCard.jsx
@@ -1,12 +1,15 @@
-import React from "react"
+import React, {useState} from "react"
 import { useNavigate } from "react-router-dom"
 import { DotsThree } from "phosphor-react"
+import ProjectMenu from "../Menus/ProjectMenu"
 
 export default function ProjectCard ({project}) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     const navigate = useNavigate()
     return (
         <div className="project-card">
-            <button><DotsThree size={"2em"}/></button>
+            {isMenuOpen && <ProjectMenu project={project}/>}
+            <button onClick={() => setIsMenuOpen(!isMenuOpen)}><DotsThree size={"2em"}/></button>
             <div onClick={() => navigate(`/projects/${project.name}`) }>
                 <h3>{project.name}</h3>
                 <p>{project.description}</p>
