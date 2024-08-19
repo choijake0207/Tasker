aa94e16b6677af6cfda3cc996bfdcc360de0c3b0
choijake0207 <jake.ch0712@gmail.com>
2024-08-19 15:42:22 -0700

    add project edit form toggle, wrap everything in fractal so form positions relative to home not card

--- src/Components/Home/ProjectCard.jsx
+++ src/Components/Home/ProjectCard.jsx
@@ -1,19 +1,31 @@
 import React, {useState} from "react"
 import { useNavigate } from "react-router-dom"
 import { DotsThree } from "phosphor-react"
-import ProjectMenu from "../Menus/ProjectMenu"
+import ProjectForm from "../Forms/ProjectForm"
+import MenuModal from "../Menus/MenuModal"
 
 export default function ProjectCard ({project}) {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isEditOn, setIsEditOn] = useState(false)
     const navigate = useNavigate()
     return (
-        <div className="project-card">
-            {isMenuOpen && <ProjectMenu project={project}/>}
-            <button onClick={() => setIsMenuOpen(!isMenuOpen)}><DotsThree size={"2em"}/></button>
-            <div onClick={() => navigate(`/projects/${project.name}`) }>
-                <h3>{project.name}</h3>
-                <p>{project.description}</p>
+        // wrapped in fragment to make edit form absolute relative to home not card
+        <> 
+            {isEditOn && 
+                <ProjectForm 
+                    onClose={() => setIsEditOn(false)} 
+                    project={project} 
+                    isEdit={true}
+                />
+            }
+            <div className="project-card">
+                {isMenuOpen && <MenuModal type="project" project={project} toggleEdit={() => setIsEditOn(!isEditOn)}/>}
+                <button className="toggle-menu-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}><DotsThree size={"2em"}/></button>
+                <div className="project-card-text"onClick={() => navigate(`/projects/${project.name}`) }>
+                    <h3>{project.name}</h3>
+                    <p>{project.description}</p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
