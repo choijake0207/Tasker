a6b39d9ba544a11a1137ad1085f7fb9e42d631fe
choijake0207 <jake.ch0712@gmail.com>
2024-08-19 14:09:04 -0700

    import deleteProj function from context

--- src/Components/Menus/ProjectMenu.jsx
+++ src/Components/Menus/ProjectMenu.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {ProjectContext} from "../../Context/ProjectContext"
 
-export default function ProjectMenu() {
+export default function ProjectMenu({project}) {
+    const {deleteProject} = useContext(ProjectContext)
   return (
     <div className="popup-menu project-menu">
         <button className="popup-edit-btn">Edit</button>
-        <button className="popup-delete-btn" onClick={() => deleteColumn(content.id)}>Delete</button>
+        <button className="popup-delete-btn" onClick={() => deleteProject(project.id)}>Delete</button>
     </div>
   )
 }
