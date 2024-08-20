52de997c93fb4da6ca2b9243fa5fc1b4dc6bafc0
choijake0207 <jake.ch0712@gmail.com>
2024-08-20 11:53:37 -0700

    wrap board in droppable

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -6,6 +6,7 @@ import { PlusCircle, FolderOpen, Folder} from 'phosphor-react'
 import ColumnForm from '../Forms/ColumnForm'
 import { DndContext } from '@dnd-kit/core'
 import { ProjectContext } from '../../Context/ProjectContext'
+import Droppable from '../../Context/DnD/Droppable'
  
 export default function Board({content}) {
   const [isFormOpen, setIsFormOpen] = useState(false)
@@ -21,40 +22,42 @@ export default function Board({content}) {
     } 
   }
 
- 
+  
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      <div className="board">
-        {isFormOpen && <ColumnForm onClose={() => setIsFormOpen(false)} content={content}/>}
-        <header className="board-header">
-          <h2>{content.name}</h2>
-          <div className="board-toolbar">
-            <button className="add-column-btn" onClick={() => setIsFormOpen(true)}><PlusCircle/>Add Column</button>
-          </div>
-        </header>
-        {content.columns.length > 0 ? 
-          <main className="board-content">
-            {content.columns.map((column) => {
-              return (
-                <Column
-                  key={column.id}
-                  column={column}
-                  content={content}
-                  tasks={column.tasks}
-                />
-              )
-            })}
-          </main> 
-          : 
-          <main className="board-content empty">
-            <FolderOpen size="8em"/>
-            <h3>Nothing Here To See Yet</h3>
-            <p>Get started by <span className="highlight-text" onClick={()=>setIsFormOpen(true)}>Creating Columns</span>
-            </p>
-          </main>
-        }
-      </div>
+      <Droppable isBoard={true} boardId={content.id}>
+        <div className="board">
+          {isFormOpen && <ColumnForm onClose={() => setIsFormOpen(false)} content={content}/>}
+          <header className="board-header">
+            <h2>{content.name}</h2>
+            <div className="board-toolbar">
+              <button className="add-column-btn" onClick={() => setIsFormOpen(true)}><PlusCircle/>Add Column</button>
+            </div>
+          </header>
+          {content.columns.length > 0 ? 
+            <main className="board-content">
+              {content.columns.map((column) => {
+                return (
+                  <Column
+                    key={column.id}
+                    column={column}
+                    content={content}
+                    tasks={column.tasks}
+                  />
+                )
+              })}
+            </main> 
+            : 
+            <main className="board-content empty">
+              <FolderOpen size="8em"/>
+              <h3>Nothing Here To See Yet</h3>
+              <p>Get started by <span className="highlight-text" onClick={()=>setIsFormOpen(true)}>Creating Columns</span>
+              </p>
+            </main>
+          }
+        </div>
+      </Droppable>
     </DndContext>
   )
 }
