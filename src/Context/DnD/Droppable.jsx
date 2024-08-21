0de2cb83e55b09d1cb696a1fd6e8628f1174b1d9
choijake0207 <jake.ch0712@gmail.com>
2024-08-20 17:34:30 -0700

    add switch case for droppable id based on type prop

--- src/Context/DnD/Droppable.jsx
+++ src/Context/DnD/Droppable.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useDroppable } from '@dnd-kit/core'
+import "./dnd.css"
 
-export default function Droppable({children, columnId, isBoard, boardId}) {
-    const id = isBoard ? `board/${boardId}` : `column/${columnId}`
-    const {isOver, setNodeRef} = useDroppable({
-        id: id
-    })
+export default function Droppable({children, columnId, isBoard, type, boardId}) {
+  let typeId
+  switch (type) {
+    case "column":
+      typeId = `column/${columnId}`;
+      break;
+    case "board":
+      typeId = `board/${boardId}`
+      break;
+    case "task":
+      typeId = `task/${taskId}`
+      break;
+  }
+  const {isOver, setNodeRef} = useDroppable({
+      id: typeId
+  })
 
     
   return (
