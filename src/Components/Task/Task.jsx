89152f8747205f862860eaa9da7a490025f97469
choijake0207 <jake.ch0712@gmail.com>
2024-08-20 18:00:08 -0700

    wrap task in droppable dnd wrapper

--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -5,6 +5,7 @@ import { Trash, Pencil, DotsThreeOutlineVertical } from 'phosphor-react'
 import Draggable from '../../Context/DnD/Draggable'
 import MenuModal from "../Menus/MenuModal"
 import TaskForm from '../Forms/TaskForm'
+import Droppable from '../../Context/DnD/Droppable'
 
 
 export default function Task({task, content, column}) {
@@ -27,37 +28,39 @@ export default function Task({task, content, column}) {
         />
       }
       <Draggable taskId={task.id}>
-        <div className="task">
-          {isMenuOpen && 
-            <MenuModal
-              onClose={() => setIsMenuOpen(false)}
-              type="task"
-              toggleEdit={() => setIsEditOn(true)}
-              project={content}
-              task={task}
-              column={column}
-            />
-          }
-          <header className="task-header">
-            <div className="task-header-tags">
-              <p className={`task-priority ${task.priority}`}>{task.priority}</p>
-              {task.category && <p className="task-category">{task.category}</p> }
-            </div>
-            <button 
-              className="task-edit-btn" 
-              onClick={handleEditButtonClick} 
-              onPointerDown={(e) => e.stopPropagation()}
-            >
-              <DotsThreeOutlineVertical size="1.2em"weight="bold"/>
-            </button>
-          </header>
-          <section className="task-details">
-            <h4 className="task-info">{task.info}</h4>
-          </section>
-          <footer className="task-footer">
-            <p className="task-date">Date Created</p>
-          </footer>
-        </div>
+        <Droppable type="task" taskId={task.id}>
+          <div className="task">
+            {isMenuOpen && 
+              <MenuModal
+                onClose={() => setIsMenuOpen(false)}
+                type="task"
+                toggleEdit={() => setIsEditOn(true)}
+                project={content}
+                task={task}
+                column={column}
+              />
+            }
+            <header className="task-header">
+              <div className="task-header-tags">
+                <p className={`task-priority ${task.priority}`}>{task.priority}</p>
+                {task.category && <p className="task-category">{task.category}</p> }
+              </div>
+              <button 
+                className="task-edit-btn" 
+                onClick={handleEditButtonClick} 
+                onPointerDown={(e) => e.stopPropagation()}
+              >
+                <DotsThreeOutlineVertical size="1.2em"weight="bold"/>
+              </button>
+            </header>
+            <section className="task-details">
+              <h4 className="task-info">{task.info}</h4>
+            </section>
+            <footer className="task-footer">
+              <p className="task-date">Date Created</p>
+            </footer>
+          </div>
+        </Droppable>
       </Draggable>
     </>
   )
