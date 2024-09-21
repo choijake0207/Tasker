a92d5d2f4e4fa4e36bf7dd497327af771229d990
choijake0207 <jake.ch0712@gmail.com>
2024-09-21 14:16:46 -0700

    configure deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  return {
   plugins: [react()],
-
+  base: mode === 'production' ? '/Tasker/' : '/',  
+  }
 })
 
