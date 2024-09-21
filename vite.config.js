4abb78a5ac3f74d6cb3a1c9c3a73b522126e273b
choijake0207 <jake.ch0712@gmail.com>
2024-09-21 14:38:57 -0700

    fix deployment paths

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
   return {
+  base: "/Tasker",
   plugins: [react()],
-  base: mode === 'production' ? './Tasker/' : './',  
+
   }
 })
 
